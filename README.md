3400d59138c1f9368c55f757c04298f27ba21983
Davin Taddeo <davin@chef.io>
2020-06-20 20:21:25 -0400

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# resource_development
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: resource_development
+# Recipe:: default
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
